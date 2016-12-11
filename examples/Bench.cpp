358381c8704c2eaf75cdfc8c56e5dd77841a0f2b
Ben Harper <rogojin@gmail.com>
2016-12-11 04:24:40 +0200

    Update benchmark

--- examples/Bench.cpp
+++ examples/Bench.cpp
@@ -9,12 +9,15 @@ void xoMain(xoSysWnd* wnd)
 	for (int y = 0; y < ny; y++)
 	{
 		xoDomNode* line = doc->Root.AddNode(xoTagDiv);
-		line->StyleParse("width: 100%; height: 1%; margin: 1px; display: block;");
+		line->StyleParse("width: 100%; height: 4px; margin: 1px; display: block;");
 		for (int x = 0; x < nx; x++)
 		{
 			xoDomNode* div = line->AddNode(xoTagDiv);
 			div->StyleParse("width: 1%; height: 100%; border-radius: 2px; margin: 1px; display: inline;");
 			div->StyleParsef("background: #%02x%02x%02x%02x", (uint8)(x * 5), 0, 0, 255);
+			char txt[2] = "a";
+			txt[0] = 'a' + x % 26;
+			div->AddText(txt);
 		}
 	}
 }
